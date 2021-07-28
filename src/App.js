7f34f4414faa4b032ae269da06b416445fa4034b
Ela <ela.git89@gmail.com>
2021-07-28 11:18:50 +0200

    dodanie strony

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Navigation} from './Navigation';
+import {Selector} from './Selector';
+import {Footer} from './Footer'
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Start} from './subpages-components/Start';
+import {ApisMelifera} from './subpages-components/ApisMelifera';
+import {Anatomy} from './subpages-components/Anatomy';
+import {SocialStructure} from './subpages-components/SocialStructure';
+import {LifeCycle} from './subpages-components/LifeCycle';
+import {YearForBees} from './subpages-components/YearForBees';
+import {Products} from './subpages-components/Products';
+import {Quiz} from './subpages-components/Quiz';
+import {Provider} from 'react-redux';
+import dataStore from './store';
+
+
+export default class App extends Component {
+  render(){
+    return(
+      <Provider store = {dataStore}>
+        <Router>
+          <header>
+              <Navigation/>
+          </header>
+          <section>
+            <Selector>
+              <Start />
+              <ApisMelifera />
+              <Anatomy />
+              <SocialStructure />
+              <LifeCycle />
+              <YearForBees />
+              <Products />
+              <Quiz />
+            </Selector>           
+          </section>
+          <Footer />
+        </Router>
+      </Provider> 
+    )
+  } 
+}
